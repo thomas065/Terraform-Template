c36b448b0c6c485b005f4e30967f34b579bf6eeb
Thomas Bell <thomasjbell065@gmail.com>
2025-05-12 10:14:29 -0400

    first commit

---
+++ 0-authentication.tf
@@ -0,0 +1,6 @@
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs
+provider "google" {
+  project = "true-artwork-456400-g8"
+  region  = "us-central1"
+  credentials = "true-artwork-456400-g8-cc40835194a7.json"
+}
